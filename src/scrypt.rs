48966968f161d386c3db681937534b2a9f5c5039
Bartosz Różański <bartek.roza@gmail.com>
2023-03-14 14:34:21 +0100

    repr(C) ScryptParams

--- src/scrypt.rs
+++ src/scrypt.rs
@@ -1,17 +1,20 @@
+/// Scrypt algorithm parameters.
+///
+/// Refer to <https://www.rfc-editor.org/rfc/rfc7914#section-2>
+#[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub struct ScryptParams {
+    /// N = 1 << (nfactor + 1)
     nfactor: u8,
+    /// r = 1 << rfactor
     rfactor: u8,
+    /// p = 1 << pfactor
     pfactor: u8,
 }
 
 impl ScryptParams {
     /// Create new ScryptParams.
     /// Note that the paramters are not N, r and p but _factors_.
-    /// The underlaying scrypt-jane library calculates them as:
-    /// - N = 1 << (nfactor + 1)
-    /// - r = 1 << rfactor
-    /// - p = 1 << pfactor
     pub fn new(nfactor: u8, rfactor: u8, pfactor: u8) -> Self {
         Self {
             nfactor,
